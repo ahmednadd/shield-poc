312de07a6e8a5531bbc8f5bee2645059b4546afe
Nadeem Ahmed <ahmed.nadd@outlook.com>
2025-03-09 02:27:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ReportDetails from './components/ReportDetails/ReportDetails'
+
+function App() {
+
+  return (
+    <div className="shield-app-container">
+      <ReportDetails />
+    </div>
+  )
+}
+
+export default App
